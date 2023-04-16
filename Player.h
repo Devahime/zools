d4582abe18f4cc0be6c33c08f38a5911dfee7de1
Jakub Kaderka <xkaderk2@mendelu.cz>
2023-04-16 16:52:37 +0200

    Created header file for class Player

---
+++ Player.h
@@ -0,0 +1,57 @@
+//
+// Created by xkapp on 16.04.2023.
+//
+
+#ifndef ZOOLS2023_XKADERK2_PLAYER_H
+#define ZOOLS2023_XKADERK2_PLAYER_H
+
+#include <string>
+#include <vector>
+
+
+class Player{
+private:
+    std::string m_name;
+    int m_health;
+    int m_armor;
+    int m_strenght;
+    std::vector<Ability*>;
+    Armor* m_armorSlot;
+    Relic* m_relicSlot;
+    Weapon* m_weaponSlot;
+
+public:
+
+    Player(std::string name);
+
+    int getHealth();
+
+    int getStrenght();
+
+    int getArmor();
+
+    void equipArmor(Armor* armor);
+
+    void dropArmor();
+
+    void equipRelic(Relic* relic);
+
+    void dropRelic();
+
+    void equipWeapon(Weapon* weapon);
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif //ZOOLS2023_XKADERK2_PLAYER_H
