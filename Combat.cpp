1d2563ff68efe8409052857b6c192b5020e94308
Jakub Kaderka <xkaderk2@mendelu.cz>
2023-04-24 14:43:31 +0200

    Created class Combat with limited functionality

---
+++ Combat.cpp
@@ -0,0 +1,23 @@
+//
+// Created by xkapp on 24.04.2023.
+//
+
+#include "Combat.h"
+
+GameLogic::Combat::Combat() {
+    reset();
+}
+
+void GameLogic::Combat::beginCombat(Player::Player *player, Entities::Enemy *enemy) {
+    m_turn = 1; //maybe will be needed to change it to zero rather
+    m_playersTurn = true;
+    m_player = player;
+    m_enemy = enemy;
+}
+
+void GameLogic::Combat::reset() {
+    m_turn = 0;
+    m_playersTurn = false;
+    m_player = nullptr;
+    m_enemy = nullptr;
+}
---
+++ Combat.h
@@ -0,0 +1,39 @@
+//
+// Created by xkapp on 24.04.2023.
+//
+
+#ifndef ZOOLS2023_XKADERK2_COMBAT_H
+#define ZOOLS2023_XKADERK2_COMBAT_H
+
+#include "Player.h"
+#include "Enemy.h"
+
+
+namespace GameLogic {
+    class Combat{
+        /**
+         * Class used for combat
+         * Combat happens only in this class. Instance of this class is in Class Game.
+         * Every time combat occures this class is used.
+         * Parametres of this class is the player, enemy and turn information.
+         * Every class that in some way needs adn uses turn information (for example ability cooldown) gets the information
+         * about turn from this class
+         * //todo describe procces of combat
+         * Once combat is over (either Player or Monster dies) the parameters of the class are reset.
+         */
+
+        int m_turn;
+        bool m_playersTurn;
+        Player::Player* m_player;
+        Entities::Enemy* m_enemy;
+
+    public:
+        Combat();
+
+        void beginCombat(Player::Player* player, Entities::Enemy* enemy);
+
+        void reset(); //maybe will be set as private
+    };
+}
+
+#endif //ZOOLS2023_XKADERK2_COMBAT_H
