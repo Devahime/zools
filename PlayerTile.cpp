32ba7e1e6d604a6666dee34f9677b4d1da97bc36
xinsanic <xinsanic@mendelu.cz>
2023-04-28 23:35:34 +0200

    added playertile.h/cpp, edited itemtile.h/cpp

---
+++ PlayerTile.cpp
@@ -0,0 +1,8 @@
+//
+// Created by xinsanic on 28.04.2023.
+//
+
+#include "PlayerTile.h"
+
+namespace Map {
+} // Map
---
+++ PlayerTile.h
@@ -0,0 +1,18 @@
+//
+// Created by xinsanic on 28.04.2023.
+//
+
+#ifndef ZOOLS2023_XKADERK2_PLAYERTILE_H
+#define ZOOLS2023_XKADERK2_PLAYERTILE_H
+
+namespace Map {
+    class PlayerTile {
+    public:
+        void print(){
+        }
+
+    };
+
+}
+
+#endif //ZOOLS2023_XKADERK2_PLAYERTILE_H
